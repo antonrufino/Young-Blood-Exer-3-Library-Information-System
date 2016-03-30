1262c0c99f60f493e51cb433b69aa6a15cc1a1ff
Anton Rufino <anton.vongola@gmail.com>
2016-03-30 10:38:29 +0800

    Main menu implemented.

---
+++ src/Library.java
@@ -0,0 +1,52 @@
+/**
+ * Class name: Library
+ * Description: This will hold the main file for the program.
+ */
+
+import java.util.Scanner;
+
+public class Library {
+    public static void main (String[] args) {
+        mainMenu();
+    }
+
+    private static void printMainMenu() {
+        System.out.println("[1] Borrow");
+        System.out.println("[2] Return");
+        System.out.println("[0] Exit");
+
+        System.out.print("Choice: ");
+    }
+
+    private static void mainMenu() {
+        int choice = -1;
+        Scanner scanner = new Scanner(System.in);
+
+        while (choice != 0) {
+            printMainMenu();
+
+            try {
+                choice = scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("Input is not a number.");
+                System.exit(1);
+            }
+
+            switch (choice) {
+                case 1:
+                    // TODO: Borrow book ui here.
+                    System.out.println("Borrow coming soon.");
+                    break;
+                case 2:
+                    // TODO: Retun book ui here.
+                    System.out.println("Return coming soon.");
+                    break;
+                case 0:
+                    System.out.println("Good bye.");
+                    break;
+                default:
+                    System.out.println("Not a valid choice");
+            }
+        }
+    }
+}
