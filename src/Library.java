77236b5f89b1ab3ce587795075326b3f65e29608
Anton Rufino <anton.vongola@gmail.com>
2016-03-30 15:53:52 +0800

    Create template for Library and User class

---
+++ src/Library.java
@@ -0,0 +1,24 @@
+/**
+ * Class name: Library
+ * Class description: This will handle library functions.
+ */
+
+public class Library {
+    // TODO: Add books attribute as a HashMap;
+
+    public Library(String dataFileName) {
+        // TODO: Load data from csv here.
+    }
+
+    public void viewBook(int id) {
+        // TODO: View individual book in the library given its id.
+    }
+
+    public void viewBook(String title) {
+        // TODO: View individual book in the library given its title.
+    }
+
+    public void viewBooks() {
+        // TODO: View available books.
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,14 @@
+/**
+ * Class name: User
+ * Class description: This handles actions by the user. 
+ */
+
+public class User {
+    // TODO: Add books borrowed as HashMap.
+
+    public User() {
+
+    }
+
+    // TODO: Add user methods
+}
