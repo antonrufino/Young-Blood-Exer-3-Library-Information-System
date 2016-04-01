fb06337ca7daa099cbc742de04f8b910a1895bdd
ariannetaglinao <lptaglinao@up.edu.ph>
2016-04-01 21:21:27 +0800

    Edited user.class

--- src/Main.java
+++ src/Main.java
@@ -1,110 +1,60 @@
-/**
- * Class name: Main
- * Description: This will hold the main file for the program.
- */
-import java.util.*;
-import java.io.*;
+import java.io.ObjectOutputStream;
+import java.io.FileOutputStream;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.ObjectInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+@SuppressWarnings("unchecked")
 
 public class Main {
-    private static Scanner scanner = new Scanner(System.in);
-    private static Library lib;
-
-    public static void main (String[] args) {
-        // mainMenu();
-        File f = new File("books.ser");
-
-        try {
-            if(f.exists()) {
-                // Retrieves books from existing file
-                System.out.println("Here!");
-                lib = new Library(new FileInputStream("books.ser"), true);
-            } else {
-                // Generates library from a csv file
-                System.out.println("Hi!");
-                FileReader fr = new FileReader("books.csv");
-                //lib = new Library(new FileReader("books.csv"));
-                lib = new Library(fr);
-            }
-        } catch(Exception e) {
-          System.out.println("OH SHIT");
-          System.exit(1);
+	public static void main(String[] args) {
+		ArrayList <User> users = new ArrayList<User>(); 	
+		
+		User user = new User("");													//creates instance of the user
+		//Deserialization
+		if (new File("users.ser").exists() == true) {								//checks if file exists
+			//reading/getting the array list of users from file (if it exists)
+			try{
+				FileInputStream fileIn = new FileInputStream("users.ser");
+				ObjectInputStream in = new ObjectInputStream(fileIn);
+				users = (ArrayList<User>) in.readObject();
+				in.close();
+				fileIn.close();
+			}
+			catch (Exception e){
+				e.printStackTrace();
+				System.out.println("Cannot read.");
+			}
+		}
+		
+		System.out.print("[1] Login\n[2] Register\n[3] Exit\nEnter choice: ");		//prints menu
+		Scanner sc = new Scanner(System.in);
+		int choice = sc.nextInt();							
+		int size = users.size();													//gets the size of the array list of users
+		
+		if (choice == 1) user.login(users, size);									//allows user to login
+		else if (choice == 2) user.register(users, size);							//allows user to register
+		
+		
+		//Serialization
+		try {
+			//writing/saving information of the users in the arraylist
+            FileOutputStream fileOut = new FileOutputStream("users.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(users);
+            out.close();
+            fileOut.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        lib.viewBooks();
-        lib.saveToFile();
-    }
-
-    private static void printMainMenu() {
-        System.out.println("[1] Borrow");
-        System.out.println("[2] Return");
-        System.out.println("[3] View books in library");
-        System.out.println("[4] View borrowed books");
-        System.out.println("[0] Exit");
-
-        System.out.print("Choice: ");
-    }
-
-    private static void mainMenu() {
-        int choice = -1;
-
-        while (choice != 0) {
-            printMainMenu();
-
-            try {
-                choice = scanner.nextInt();
-            } catch (Exception e) {
-                System.out.println("Input is not a number.");
-                System.exit(1);
-            }
-
-            switch (choice) {
-                case 1:
-                    // TODO: Borrow book ui here.
-                    borrowMenu();
-                    break;
-                case 2:
-                    // TODO: Retun book ui here.
-                    returnMenu();
-                    break;
-                case 3:
-                    // TODO: View books ui here.
-                    System.out.println("View books coming soon.");
-                    break;
-                case 4:
-                    // TODO: View borrowed books ui here.
-                    System.out.println("View borrowed books coming soon.");
-                    break;
-                case 0:
-                    System.out.println("Good bye.");
-                    break;
-                default:
-                    System.out.println("Not a valid choice");
-            }
+        catch (Exception e) {
+            e.printStackTrace();
         }
-    }
-
-    private static void borrowMenu() {
-        String title;
-
-        System.out.print("Book title: ");
-        scanner.nextLine();
-        title = scanner.nextLine();
-
-        // TODO: Borrow implementation goes here.
-
-        System.out.println("You borrowed " + title + ".");
-    }
-
-    private static void returnMenu() {
-        String title;
-
-        System.out.print("Book title: ");
-        scanner.nextLine();
-        title = scanner.nextLine();
-
-        // TODO: Borrow implementation goes here.
-
-        System.out.println("You returned " + title + ". Thank you.");
-    }
-
+	}
 }
--- src/User.java
+++ src/User.java
@@ -1,14 +1,167 @@
+import java.io.Serializable;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.Scanner;
 /**
  * Class name: User
  * Class description: This handles actions by the user. 
  */
 
-public class User {
-    // TODO: Add books borrowed as HashMap.
+public class User implements Serializable {
+	private String username;
+    private String password;
+    private String bookTitle;
+    public String name;
+    private int checker = 0;
+    private int a;
+  	private static final long serialVersionUID = 1L;
 
-    public User() {
-
+    public User(String name) {										//constructor of a user
+    	this.name = name;
     }
-
-    // TODO: Add user methods
+    
+    public void borrowBook() {
+    	System.out.println("\nBorrow Books");
+    	
+    	Scanner sc = new Scanner(System.in);
+    	Scanner sc2 = new Scanner(System.in);
+    	Library.viewBooks();
+    	
+    	System.out.println("\nHow many books you want to borrow?");
+    	a = sc.nextInt();
+    	
+    	System.out.println("Enter title of book/s: ");
+    	while (a != 0) {
+	    	bookTitle = sc2.nextLine();
+	    	a -= 1;
+	    	try {
+	    		File f = new File("user.txt");
+	    		BufferedWriter writer = new BufferedWriter(new FileWriter(f, true));
+	    		writer.writeLine(get(bookTitle).get(0));
+	    		
+	    		writer.close();
+	    	}
+	    	catch (Exception ex) {
+	    		System.out.println("Error!");
+	    	}
+	    	
+	    	get(bookTitle).remove(0);
+    	}
+    }
+    
+    public void returnBook() {
+    	System.out.println("\nReturn Books");
+    	
+    	Scanner sc = new Scanner(System.in);
+    	Scanner sc2 = new Scanner(System.in);
+    	
+    	System.out.println("\nHow many books you want to return?");
+    	a = sc.nextInt();
+    	
+    	System.out.println("Enter title of book/s: ");
+    	while (a != 0) {
+	    	bookTitle = sc2.nextLine();
+	    	a -= 1;
+	    	
+	    	try {
+	    		File f = new File("user.txt");
+	    		BufferedReader reader = new BufferedReader(new FileReader(f));
+	    		
+	    		remove(bookTitle);
+	    		
+	    		reader.close();
+	    	}
+	    	catch (Exception ex) {
+	    		System.out.println("Error!");
+	    	}
+    	}
+    	
+    }
+    
+    public void userBooks() {
+    	try {
+	    	File f = new File("user.txt");
+	    	BufferedReader reader = new BufferedReader(new FileReader(f));
+	    	String line;
+	    	while ((line = reader.readLine()) != null) {
+	    		System.out.println(line);
+	    	}
+	    	reader.close();
+	    }
+	    catch (Exception ex) {
+	    	System.out.println("Error!");
+	    }
+    }
+    
+    public void setInfo(String username, String password) {			//function for setting the username and password of the account of user
+    	this.username = username;
+   	 	this.password = password;
+   	}
+    
+    public String getUsername() {									//getter for username
+    	return this.username;
+    }
+    
+    public String getPassword() {									//getter for password
+    	return this.password;
+    }
+	
+	public void login(ArrayList<User> users, int size) {			//method that allows user to login
+		System.out.println("\nLogin");
+		Scanner sc = new Scanner(System.in);
+		checker = 0;
+		while (checker != 2) {
+			System.out.print("Username: ");							//gets the username		
+			username = sc.nextLine();	
+			System.out.print("Password: ");							//gets the password
+			password = sc.nextLine();
+			
+			this.setInfo(username, password);	
+			
+			//validate if the username exists and if the password matches the username
+			for (int i=0; i<size; i++) {
+				if (this.getUsername().equalsIgnoreCase(users.get(i).getUsername()) == true && this.getPassword().equals(users.get(i).getPassword())) {
+					checker = 1;
+					break;	
+				}
+			}
+			if (checker == 0) System.out.println("Incorrect username and/or password");
+			else checker = 2;
+		}
+	}
+	
+	public void register(ArrayList<User> users, int size) {			//method that allows user to register/make an account
+		System.out.println("\nRegister");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Username: ");	
+		username = sc.nextLine();
+		
+		//checks if username already exists
+		while (checker != 2) {
+			for (int i=0; i<size; i++) {
+				if (username.equalsIgnoreCase(users.get(i).getUsername()) == true) {
+					checker = 1;
+					break;	
+				}
+			}
+			if (checker == 1) {
+				System.out.println("Username already exists");
+				System.out.print("Username: ");	
+				username = sc.nextLine();
+				checker = 0;
+			} else checker = 2;
+		}
+		System.out.print("Password: ");
+		password = sc.nextLine();
+			
+		this.setInfo(username, password);								//sets the username and password of the account of user
+		users.add(this);												//adds the user to the list of all users
+		System.out.println("Successful");
+	}
 }
